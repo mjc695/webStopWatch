68a09d6bbe7120065f3fdc43bcdae45fb5a7ae24
Monferd <monferd.j.colin@gmail.com>
2021-02-04 14:16:26 -0500

    pushing for work on desktop, adding history modification. logic for updating navbar not finished

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import {Link, useHistory} from 'react-router-dom'
 
 const Navbar = () =>{
+
+    const history = useHistory()
+
+    const [userInfo, setUserInfo] = useState()
+    
+    useEffect(()=>{
+        const userData = window.localStorage.getItem('userInfo')
+        if(userData){
+            console.log('userData:', userData)
+            setUserInfo(userData)
+        }
+    })
 
     const logout = () =>{
         window.localStorage.removeItem('userInfo')
         console.log('logged out:', window.localStorage.getItem('userInfo'))
+        history.push('/homepage')
     }
 
     return(
         <div>
             Navbar
+            {userInfo? 
             <button type='submit' onClick={logout}> Logout </button>
-            <Link to='/signIn'>Sign In </Link>
+            : <Link to='/signIn'>Sign In </Link>
+        }
         </div>
     )
 }
